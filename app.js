a3ef4bd2ea283d429bd57b5b5ac02e4fcd799d17
suzhihui <wisdom.su@163.com>
2017-03-24 10:24:22 +0800

    lxx

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 var express = require('express')
 var path = require('path')
 var _ = require('underscore')
-
+var sequelize = require('./config/server')
 
 
 
@@ -16,7 +16,19 @@ var app = express()
 app.set('views', './app/views/pages')
 app.set('view engine', 'jade')
 app.use(express.bodyParser())
+app.use(express.cookieParser())
+app.use(express.session({
+  secret: 'laosu',
+  store: ''
+}))
 
+// development: 开发环境
+if ('development' === app.set('env')) {
+  app.set('showStackError', true)
+  app.use(express.logger(':method :url :status'))
+  app.locals.pretty = true
+  // sequelize.set('debug', true)
+}
 
 require('./config/routes')(app)
 app.use(express.static(path.join(__dirname, 'public')))
